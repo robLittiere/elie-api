1e053d7a66fc69aaf217c55be60aba7c5a787053
Robin Littiere <zauts95@gmail.com>
2024-01-19 16:37:21 +0100

    updated to quiz query to game version

--- modules/game/infrastructure/quiz.go
+++ modules/game/infrastructure/quiz.go
@@ -44,7 +44,7 @@ func (r *QuizRepo) BuildQueryAndFind(queryParams map[string][]string) ([]models.
 		Select("quiz").
 		Joins("JOIN jsonb_array_elements(data->'topic') as topic ON TRUE").
 		Joins("JOIN jsonb_array_elements(topic->'quizzes') as quiz ON TRUE").
-		Where("SELECT id FROM games WHERE name = ? AND game_version = ?", "Quiz", "1.0")
+		Where("gid = ?", 4)
 
 	err := r.BuildQuery(queryParams)
 	if err != nil {
