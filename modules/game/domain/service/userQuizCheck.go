3b8d35ca2f34371fd086a0dcbc2ebbe19e12ca7f
Paul Geneve <geneve.paul1295@gmail.com>
2024-04-10 11:31:37 +0200

    fix comment PR Rob

--- modules/game/domain/service/userQuizCheck.go
+++ modules/game/domain/service/userQuizCheck.go
@@ -11,16 +11,16 @@ type UserQuizService struct {
 	QuizRepo *infrastructure.QuizRepo
 }
 
-func (s *UserQuizService) CheckUserQuizExist(userQuiz models.UserQuiz) (bool, error) {
+func (s *UserQuizService) UserQuizExists(userQuiz models.UserQuiz) (bool, error) {
 	var existQuiz int64
 	var quizGameId int
 	result := s.QuizRepo.DB.Table("user_quizzes").Where("user_id = ? AND quiz_id = ?", userQuiz.UserID, userQuiz.QuizId).Count(&existQuiz)
 	fmt.Printf("quizGameId: %v\n", quizGameId)
 
-	if result.Error != nil {
-		return true, result.Error
+	if result.Error != nil || existQuiz > 0 {
+		return false, result.Error
 	}
-	return false, nil
+	return true, nil
 }
 
 func NewUserQuizService(db *gorm.DB) *UserQuizService {
