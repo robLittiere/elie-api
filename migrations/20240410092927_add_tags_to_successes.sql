b8705cad89f81cf9bba437ece160f1acaf8c78fb
Robin Littiere <zauts95@gmail.com>
2024-04-10 12:35:14 +0200

    add tags to quests

--- migrations/20240410092927_add_tags_to_successes.sql
+++ migrations/20240410092927_add_tags_to_successes.sql
@@ -2,7 +2,7 @@
 -- +goose StatementBegin
 ALTER TABLE successes ADD COLUMN IF NOT EXISTS tags varchar(255) DEFAULT NULL;
 UPDATE successes SET tags = 'login' WHERE name LIKE '%Se connecter%';
-UPDATE successes SET tags = 'quiz' WHERE name LIKE '%quiz%';
+UPDATE successes SET tags = 'quiz_complete' WHERE name LIKE '%quiz%';
 UPDATE successes SET tags = 'level' WHERE name LIKE '%niveau%';
 UPDATE successes SET tags = 'avatar' WHERE name LIKE '%avatar%';
 
--- modules/gamification/models/quest.go
+++ modules/gamification/models/quest.go
@@ -11,7 +11,7 @@ type Quest struct {
 	Difficulty     string    `json:"difficulty"`
 	CurrencyReward int       `json:"currency_reward" gorm:"default:0"`
 	DoneCondition  int       `json:"done_condition"`
-	Tags           []string  `json:"tags" gorm:"type:text[]"`
+	Tags           string    `json:"tags"`
 	CreatedAt      time.Time `json:"createdAt"`
 	UpdatedAt      time.Time `json:"updatedAt"`
 }
